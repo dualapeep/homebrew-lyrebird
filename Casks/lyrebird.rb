52d53b9e4042c8cba6f7ade39af0df2c19aac232
lyrebird-ci <ci@lyrebird.app>
2026-06-17 13:20:18 +0000

    lyrebird 0.1.22

--- Casks/lyrebird.rb
+++ Casks/lyrebird.rb
@@ -1,6 +1,6 @@
 cask "lyrebird" do
-  version "0.1.21"
-  sha256 "51b060a608e938b944fb4f36133ca8c72bd6cc90686014d3cee63501895b99e7"
+  version "0.1.22"
+  sha256 "3a6c2e3f0f7c9955ec31f0b43a9aa3b3bf3d331d9f2ce0f46d5588e51817a28a"
 
   url "https://d21pmv6rhp0qg2.cloudfront.net/downloads/Lyrebird-#{version}-arm64.dmg"
   name "Lyrebird"
