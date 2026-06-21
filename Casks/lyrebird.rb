43bef7973cdc53d448cf54f6f6fb06539f4b6a33
lyrebird-ci <ci@lyrebird.app>
2026-06-21 11:26:37 +0000

    lyrebird 0.1.43

--- Casks/lyrebird.rb
+++ Casks/lyrebird.rb
@@ -1,12 +1,12 @@
 cask "lyrebird" do
   arch arm: "arm64", intel: "x64"
 
-  version "0.1.42"
+  version "0.1.43"
   on_arm do
-    sha256 "fd0d6ad55876cbd21ab50289ecfebf76002734d9a759ef82d57956d3da5f9c24"
+    sha256 "c0611f8273ae921cc72bde164540364ffbb644aa7b5e9e6b34b37deafd17bfc5"
   end
   on_intel do
-    sha256 "b09dfeb2be55d1528741ef42a8e83e85a3bdd27af4bb3d537eb71e14ac0fd7c7"
+    sha256 "476b2b9d63a9da8c15b4ee78d04a4b9a2ca671ed20088481d38ad0f6882e218a"
   end
 
   url "https://d21pmv6rhp0qg2.cloudfront.net/downloads/Lyrebird-#{version}-#{arch}.dmg"
